9384b5165d83e521e687ae0f2fec4e0c125ac719
Mohul <83111543+Mohul@users.noreply.github.com>
2023-01-03 20:15:22 +0530

    Update Problem_1.java

--- Problem_1.java
+++ Problem_1.java
@@ -4,31 +4,20 @@ class Problem_1{
     static void main() {
         //Creating object for the Scanner class
         Scanner x = new Scanner(System.in);
-        System.out.println("Enter a 3 digit number");
-        //Taking user input
-        int a = x.nextInt();
-        //Checking whether a number is 3 digit or not
+        int n,d,s = 0,rev = 0;
+        //Excepting a 3 digit number from the user
+        System.out.println("Enter any Triple digit number");
+        n = x.nextInt();
+        //Finding the sum and reversing the number
         if(a>=100 && a<=999){
-            //Separating digits from the number
-            //Last
-            int l = a%10;
-            //First
-            int f = a/100;
-            //Middle
-            int m = (a%10)/10;
-            //Pointing out the totall of all the digits
-            int s = l+f+m;
-            System.out.println("The total of all the digits is: " + s);
-            //Reversing the number
-            int r = 0;
-            int rev = 0;
-            System.out.print("The reversed number is:\t");
             while(a!=0){
-                 r = a%10;
-                 rev = rev*10 + r;
-                 a = a/10;
+                d = n%10;
+                s = s+d;
+                rev = rev * 10 + d;
+                n/ = 10;
             }
-            System.out.println(rev);
+            System.out.println("The total of all digits: " + s);
+            System.out.println("Reverse of the number: " + rev);
         }
     }
 }
